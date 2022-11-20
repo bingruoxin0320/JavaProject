9296249ce427a013bf89f0df4e9d072efd6a10d4
bingruoxin <bingruoxin0320@163.com>
2022-11-20 23:04:53 +0800

    事件处理

--- p1/src/com/mingrisoft/FlowLayoutPosition.java
+++ p1/src/com/mingrisoft/FlowLayoutPosition.java
@@ -1,6 +1,7 @@
 package com.mingrisoft;
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.ButtonGroup;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,51 +10,71 @@ public class FlowLayoutPosition extends JFrame{
 //        super("Decode");
         setTitle("Decode");
         Container container = getContentPane();
-        container.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        setBounds(200,130,850,500);
+        container.setLayout(new BorderLayout());
+        setBounds(200,130,1200,680);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
 
 
+        // 面板1
         JPanel jPanel1 = new JPanel();
-        jPanel1.setLayout(new GridLayout(2,2,20,20));
-        jPanel1.setSize(100,80);
-        JButton encode1 = new JButton("Encode1");
-        JButton encode2 = new JButton("Encode2");
-        JButton encode3 = new JButton("Encode3");
-        JButton encode4 = new JButton("Encode4");
-        encode4.setBackground(Color.green);
-        encode4.setSize(20,20);
-        jPanel1.add(encode1);
-        jPanel1.add(encode2);
-        jPanel1.add(encode3);
-        jPanel1.add(encode4);
-        container.add(jPanel1);
+        jPanel1.setLayout(new GridLayout(1,2,10,10));
+        jPanel1.setPreferredSize(new Dimension(500,580));
+        jPanel1.setBorder(BorderFactory.createTitledBorder("面板1"));
+        // 标签
+        JLabel decodeLabel = new JLabel();
+        decodeLabel.setText("Decode");
+        decodeLabel.setLocation(10,10);
+        jPanel1.add(decodeLabel);
+        // 文本
+        JTextField textFieldLeft = new JTextField();
+        textFieldLeft.setText("输入文本...");
+//        textFieldLeft.setLocation(10,20);
+//        textFieldLeft.setSize(200,200);
+        jPanel1.add(textFieldLeft);
 
+        container.add(jPanel1,BorderLayout.WEST);
+
+        // 面板2 按钮
         JPanel jPanel2 = new JPanel();
-        jPanel2.setLayout(new BorderLayout());
-        JButton encode5 = new JButton("Encode5");
-        JButton encode6 = new JButton("Encode6");
-        JButton encode7 = new JButton("Encode7");
-        JButton encode8 = new JButton("Encode8");
-        jPanel2.add(encode5,BorderLayout.NORTH);
-        jPanel2.add(encode6,BorderLayout.SOUTH);
-        jPanel2.add(encode7,BorderLayout.EAST);
-        jPanel2.add(encode8,BorderLayout.WEST);
-        jPanel2.setLocation(250,180);
-        jPanel2.setSize(100,100);
+        jPanel2.setLayout(new GridLayout(4,1,10,10));
+        jPanel2.setBorder(BorderFactory.createTitledBorder("面板2"));
+        jPanel2.setPreferredSize(new Dimension(200,100));
+        JButton decodeButton = new JButton();
+        decodeButton.setText("Decode");
+        decodeButton.setToolTipText("面板1输入内容");
+        decodeButton.setBorderPainted(true);
+        decodeButton.setEnabled(true);
+        decodeButton.setBackground(Color.WHITE);
+        jPanel2.add(decodeButton);
+        JButton encodeButton = new JButton("Encode");
+        encodeButton.setBackground(Color.WHITE);
+        jPanel2.add(encodeButton);
+
+        JRadioButton radioButton1 = new JRadioButton("单选1",true);
+        jPanel2.add(radioButton1);
+        JRadioButton radioButton2 = new JRadioButton();
+        radioButton2.setText("单选2");
+        jPanel2.add(radioButton2);
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(radioButton1);
+        buttonGroup.add(radioButton2);
         container.add(jPanel2);
 
-        // 添加按钮
-//        container.add(new JButton("Decode").addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                BorderDialog borderDialog = new BorderDialog(FlowLayoutPosition.this);
-//            }
-//        }));
+        // 面板3
+        JPanel jPanel3 = new JPanel();
+        jPanel3.setLayout(new FlowLayout(FlowLayout.LEFT));
+        jPanel3.setBorder(BorderFactory.createTitledBorder("面板3"));
+        jPanel3.setPreferredSize(new Dimension(500,580));
+        container.add(jPanel3, BorderLayout.EAST);
+        // 标签
+        JLabel encodeLabel = new JLabel("Encode");
+        jPanel3.add(encodeLabel);
+        // 文本
+        JTextField textFieldRight = new JTextField();
+        textFieldRight.setPreferredSize(new Dimension(200,80));
+        jPanel3.add(textFieldRight);
 
-
-
+        setVisible(true);
     }
     public static void main(String[] args) {
         new FlowLayoutPosition();

61334bb653791d8ad6925fbe0a8565d96acec834
bingruoxin <bingruoxin0320@163.com>
2022-12-28 22:42:27 +0800

    文本写入

--- p1/src/FileDirectoryText/FileText.java
+++ p1/src/FileDirectoryText/FileText.java
@@ -1,7 +1,6 @@
 package FileDirectoryText;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -46,11 +45,68 @@ class TestFile {
 }
 class FolderTest {
     protected void directoryTest(){
-        String path = "E:\\ProjectFile\\JavaProject\\p1\\src";
-        for (int i = 1; i <= 10; i++) {
-            File folder = new File(path + "\\" + i);
-            if (!folder.exists()) {
-                folder.mkdir();
+        File file = new File("word.txt");
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            String content = "你见过洛杉矶凌晨4点的样子吗？";
+            byte[] buy = content.getBytes();
+            out.write(buy);
+            out.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        try {
+            FileInputStream in = new FileInputStream(file);
+            byte[] byt = new byte[1024];
+            int len = in.read(byt);
+            System.out.println(new String(byt,0,len));
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void bufferedStreamTest() {
+        String[] content = {"千呼万唤始出来","犹抱琵琶半遮面","嘈嘈切切错杂弹","大珠小珠落玉盘"};
+        File file = new File("workLookLook.txt");
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        try {
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+            byte[] bContent = new byte[1024];
+            for (int k = 0;k<content.length;k++){
+                bContent = content[k].getBytes();
+                bos.write(bContent);
+            }
+            System.out.println("写入成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bos.close();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+            byte[] bContent = new byte[1024];
+            int len = bis.read(bContent);
+            System.out.println(new String(bContent,0,len));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
